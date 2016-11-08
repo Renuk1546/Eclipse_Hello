8bccbd8a66be296b6cd93fff3203a497a89ad02e
Renuk1546 <wdruid@naver.com>
2016-11-08 15:16:16 +0900

    remote PushPrint.java modified

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java class File Push"); 
 		System.out.println("World with 365");
+		System.out.println("remote master");
 	}
 
 }
